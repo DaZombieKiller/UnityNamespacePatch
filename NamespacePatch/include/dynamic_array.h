7e33519c66f20a0d9641b7edbef2dfb139b18a68
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2023-09-18 09:03:05 +1000

    Update dynamic_array.h

--- NamespacePatch/include/dynamic_array.h
+++ NamespacePatch/include/dynamic_array.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "MemLabelId.h"
 
-struct dynamic_array_detail
+namespace dynamic_array_detail
 {
-    void* m_ptr;
-    MemLabelId m_label;
-    size_t m_size;
-    size_t m_capacity;
+    struct dynamic_array_data
+    {
+        void* m_ptr;
+        MemLabelId m_label;
+        size_t m_size;
+        size_t m_capacity;
+    };
 };
 
-template<typename T, int = 0>
-struct dynamic_array : dynamic_array_detail
+template<typename T, size_t ALIGN = 0>
+struct dynamic_array : dynamic_array_detail::dynamic_array_data
 {
 };
